a047c9eb83f81819354a825d09d99e859b1a6329
Andy Whillans <andy.whillans@gmail.com>
2018-06-26 15:30:55 -0400

    Setup react in project, create initial react component

---
+++ app/javascript/components/GuestBookView.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+class GuestBookView extends React.Component {
+  render () {
+    return (
+      <h3>Our recommended books:</h3>
+    );
+  }
+}
+
+export default GuestBookView
--- spec/features/home_spec.rb
+++ spec/features/home_spec.rb
@@ -1,9 +1,10 @@
 require 'rails_helper'
 
-describe 'visiting the home page' do
+describe 'visiting the home page', js: true do
   it 'allows the user to see a home page at the root' do
     visit '/'
 
     expect(page).to have_content('Second Treasures Bookstore')
+    expect(page).to have_content('Our recommended books:')
   end
 end 
