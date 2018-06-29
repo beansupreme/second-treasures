1d4ddee36a07650de6786840445c8762a715171d
Andy Whillans <andy.whillans@gmail.com>
2018-06-28 23:50:19 -0400

    Add validations to protect from long user inputs

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -2,12 +2,48 @@ require 'rails_helper'
 
 describe Book, type: :model do
   describe 'validations' do
-    it 'should validate presence of title' do
-      book_without_title = Book.create(title: '')
-      expect(book_without_title.valid?).to eq(false)
+    let(:book) do
+      Book.create(
+        title: 'Something'
+      )
+    end
+    let(:absurdly_long_string) do
+      1000.times.map {'A'}
+    end
+    before do
+      expect(book.valid?).to eq(true)
+    end
 
-      book_without_title = Book.create(title: 'Present')
-      expect(book_without_title.valid?).to eq(true)
+    it 'should validate presence of title' do
+      book.update(title: '')
+      expect(book.valid?).to eq(false)
+
+      book.update(title: 'Present')
+      expect(book.valid?).to eq(true)
+    end
+
+    it 'should validate max length of ISBN' do
+      book.update(isbn: '978-0440238133000000000000000000000')
+      expect(book.valid?).to eq(false)
+
+      book.update(isbn: '978-3-16-148410-0')
+      expect(book.valid?).to eq(true)
+    end
+
+    it 'should validate max length of genre' do
+      book.update(genre: absurdly_long_string)
+      expect(book.valid?).to eq(false)
+
+      book.update(genre: 'A reasonable length')
+      expect(book.valid?).to eq(true)
+    end
+
+    it 'should validate max length of author' do
+      book.update(author: absurdly_long_string)
+      expect(book.valid?).to eq(false)
+
+      book.update(author: 'A reasonable length')
+      expect(book.valid?).to eq(true)
     end
   end
 end
